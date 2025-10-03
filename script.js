98e53c425285b8ca1f2cd42187ff221c536bb237
cpons21179-prog <cpons21179@iesjoanramis.org>
2025-10-03 08:50:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ function harrypotter(){
         document.getElementById("foto").hidden = true
     }
 }
+
+function canviafons(){
+   let body = document.getElementsByTagName("body")[0]
+   let foto = prompt("DONDE QUIERES VIAJAR?")
+   body.style = "background-image: url('"+foto+"'); background-size: cover;"
+}
+
+function tradueix(){
+    document.getElementsByTagName("body")[0]
+    body.style = ""
+}
+
+function afegirInfo(){
+    let llista = document.getElementById("expL");
+    let element = document.createElement("li");
+    element.innerHTML = prompt("QUE VOLS AFEGIR")
+    llista.appendChild(element)
+}
